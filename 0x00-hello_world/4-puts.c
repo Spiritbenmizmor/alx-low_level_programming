f9719bf7f798c0cae5ad2516dbc6d2b5df4a14b4
Spiritbenmizmor <blessingnanayawayensu@gmail.com>
2022-09-13 01:29:12 -0700

    4-puts.c and the rest

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+
+ * main - Entry point
+ *
+ *this uses the main function
+ *Thid code puts ""Programming is like building a multilingual puzzle" 
+ * Return: 0
+ */
+
+
+int main(void)
+{
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+  
+}
