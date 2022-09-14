0461a99ac4d8ee255990423bfa0d7e5f1f288dc5
Spiritbenmizmor <blessingnanayawayensu@gmail.com>
2022-09-14 13:22:54 -0700

    4-putsc corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,11 @@
 *This uses the main function
 *The program prints "Programming is like a multilingual puzzle"
 *Return; 0
-
 */
+
 int main(void)
+
 {
-	puts("\"Programming is like a multilingualpuzzle");
+	puts("\"Programming is like a multilingual puzzle");
 	return (0);
 }
