2b6680f8875352efe0ce9ffb5f629dc1fba8b109
Spiritbenmizmor <blessingnanayawayensu@gmail.com>
2022-12-19 22:43:34 -0800

    Submitting 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - prints the exact string followed by a new line
+ *using the printf function
+ *Return: Always 0
+ */
+
+int main(void)
+{
+          printf("with propper grammar, but the outcome is a piece of art, \n");
+	  return (0);
+	  
+}
